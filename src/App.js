1b7ef95e37bc4f373718a1c6466de1d8bbac71c2
AndresRuiz01 <andres.ruiz3210@gmail.com>
2023-01-05 20:05:00 -0600

    add back button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import TimerManager from './TimerManager';
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-
+import IconButton from '@mui/material/IconButton';
+// import DeleteIcon from '@mui/icons-material/Delete';
+import Fingerprint from '@mui/icons-material/Fingerprint';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const teacher_timers_13 = [
   {title: "Get Ready", length: 1},
@@ -34,6 +37,12 @@ function App() {
   const [show12Timer, setShow12Timer] = useState(false);
   const [show13Timer, setShow13Timer] = useState(false);
 
+  const displayOptions = () => {
+    setShowButtonOptions(true);
+    setShow12Timer(false);
+    setShow13Timer(false);
+  }
+
   const display12MinuteTimer = () => {
     setShowButtonOptions(false);
     setShow12Timer(true);
@@ -48,12 +57,23 @@ function App() {
 
   return (
     <div className="App">
+
+      {/* Clock Options */}
       {showButtonOptions && <div>
         <Stack spacing={2} direction="row">
           <Button variant="contained" onClick={display12MinuteTimer}>12 Minute</Button>
           <Button variant="contained" onClick={display13MinuteTimer}>13 Minute</Button>
         </Stack>
       </div>}
+
+      {/* Back Button */}
+      {(show12Timer || show13Timer) && <div id="back-button">
+        <IconButton size="large" onClick={displayOptions}>Back
+          {/* <ArrowBackIcon color="primary" fontSize="large" /> */}
+        </IconButton>
+      </div>}
+
+      {/* Timers */}
       {show12Timer && <TimerManager timers={teacher_timers_12}/>}
       {show13Timer && <TimerManager timers={teacher_timers_13}/>}
     </div>
