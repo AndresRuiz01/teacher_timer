93b88aa904fa1cbde5ff1317d0af59d4241af244
Ruiz-Andres <andres-ruiz@sandhills.com>
2023-09-09 16:47:35 -0500

    add 14 minute timer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,18 @@ import IconButton from '@mui/material/IconButton';
 import Fingerprint from '@mui/icons-material/Fingerprint';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
+const teacher_timers_14 = [
+  {title: "Get Ready", length: 1},
+  {title: "Station One", length: 14},
+  {title: "Transition", length: 1},
+  {title: "Station Two", length: 14},
+  {title: "Transition", length: 1},
+  {title: "Station Three", length: 14},
+  {title: "Transition", length: 1},
+  {title: "Station Four", length: 14},
+  {title: "Clean Up", length: 2},
+]
+
 const teacher_timers_13 = [
   {title: "Get Ready", length: 1},
   {title: "Station One", length: 13},
@@ -36,24 +48,37 @@ function App() {
   const [showButtonOptions, setShowButtonOptions] = useState(true);
   const [show12Timer, setShow12Timer] = useState(false);
   const [show13Timer, setShow13Timer] = useState(false);
+  const [show14Timer, setShow14Timer] = useState(false);
+
 
   const displayOptions = () => {
     setShowButtonOptions(true);
     setShow12Timer(false);
     setShow13Timer(false);
+    setShow14Timer(false);
   }
 
   const display12MinuteTimer = () => {
     setShowButtonOptions(false);
     setShow12Timer(true);
     setShow13Timer(false);
+    setShow14Timer(false);
   }
 
   const display13MinuteTimer = () => {
     setShowButtonOptions(false);
     setShow12Timer(false);
     setShow13Timer(true);
+    setShow14Timer(false);
   }
+
+  const display14MinuteTimer = () => {
+    setShowButtonOptions(false);
+    setShow12Timer(false);
+    setShow13Timer(false);
+    setShow14Timer(true);
+  }
+
 
   return (
     <div className="App">
@@ -63,6 +88,7 @@ function App() {
         <Stack spacing={2} direction="row">
           <Button variant="contained" onClick={display12MinuteTimer}>12 Minute</Button>
           <Button variant="contained" onClick={display13MinuteTimer}>13 Minute</Button>
+          <Button variant="contained" onClick={display14MinuteTimer}>14 Minute</Button>
         </Stack>
       </div>}
 
@@ -76,6 +102,7 @@ function App() {
       {/* Timers */}
       {show12Timer && <TimerManager timers={teacher_timers_12}/>}
       {show13Timer && <TimerManager timers={teacher_timers_13}/>}
+      {show14Timer && <TimerManager timers={teacher_timers_14}/>}
     </div>
   );
 }
